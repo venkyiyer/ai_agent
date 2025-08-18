61849c23aa9ab5a4e378cb6a4a85bbcb75f5101f
venkyiyer <iyervenkatesh92@gmail.com>
2025-08-18 22:46:26 +0530

    18/8

--- tools.py
+++ tools.py
@@ -1,22 +1,29 @@
-from pydantic import BaseModel
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from dotenv import load_dotenv
-from langchain_core.prompts import ChatPromptTemplate
-from lanchain_core.output_parsers import pydantinOutputParser
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools import Tool
+from datetime import datetime
 
-load_dotenv()
+def save_to_txt(data: str, filename: str = "research_output.txt"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
 
-# llm = ChatOpenAI(model = 'gpt-4o-mini')
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(formatted_text)
+    
+    return f"Data successfully saved to {filename}"
 
-class ResearchResponse(BaseModel):
-    topic:str
-    summary: str
-    sources: list[str]
-    tools_used: list[str]
+save_tool = Tool(
+    name="save_text_to_file",
+    func=save_to_txt,
+    description="Saves structured research data to a text file.",
+)
 
+search = DuckDuckGoSearchRun()
+search_tool = Tool(
+    name="search",
+    func=search.run,
+    description="Search the web for information",
+)
 
-llm = ChatAnthropic(model = 'claude-3-5-sonnet-20241022')
-respone = llm.invoke('What is the meaning of God? ')
-
-print(respone)
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
